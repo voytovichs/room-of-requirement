0c72b634630a6711bcbc67a69429b7d66f345e15
Sergey Voytovich <sergey@xix.ai>
2019-11-16 14:59:44 +0000

    Tests for SniffingInputStream and SniffingOutputStream

--- src/io/kttest/com/ror/io/SniffingInputStreamTest.kt
+++ src/io/kttest/com/ror/io/SniffingInputStreamTest.kt
@@ -1,4 +1,44 @@
 package com.ror.io
 
+import org.junit.jupiter.api.Assertions.assertArrayEquals
+import org.junit.jupiter.api.Test
+import java.io.ByteArrayInputStream
+
 class SniffingInputStreamTest {
+    @Test
+    fun data_from_host_stream_is_not_lost_test() {
+        val expected = data.toByteArray()
+        val input = ByteArrayInputStream(expected)
+
+        val sniffingInput = SniffingInputStream(input)
+        val actual = ByteArray(expected.size)
+
+        val halfSize = actual.size / 2
+        sniffingInput.read(actual, 0, halfSize)
+        sniffingInput.read(actual, halfSize, actual.size - halfSize)
+
+        assertArrayEquals(expected, actual)
+    }
+
+    @Test
+    fun sniffed_data_is_equal_to_actual_data_test() {
+        val expected = data.toByteArray()
+        val input = ByteArrayInputStream(expected)
+
+        val sniffingInput = SniffingInputStream(input)
+        val actual = ByteArray(expected.size)
+
+        val halfSize = actual.size / 2
+        sniffingInput.read(actual, 0, halfSize)
+        sniffingInput.read(actual, halfSize, actual.size - halfSize)
+
+        assertArrayEquals(expected, sniffingInput.content)
+    }
+
+    private val data = buildString {
+        appendln("The data this is")
+        appendln("This is a second line")
+        appendln("I will not lose my sniffing streams no more")
+        appendln("One less distraction obscures the skyline")
+    }
 }
--- src/io/kttest/com/ror/io/SniffingOutputStreamTest.kt
+++ src/io/kttest/com/ror/io/SniffingOutputStreamTest.kt
@@ -1,4 +1,47 @@
 package com.ror.io
 
+import org.junit.jupiter.api.Assertions.assertArrayEquals
+import org.junit.jupiter.api.Test
+import java.io.ByteArrayOutputStream
+import java.io.OutputStream
+
 class SniffingOutputStreamTest {
+    @Test
+    fun data_reaches_host_stream_test() {
+        val bytes = data.toByteArray()
+        val half = bytes.size / 2
+
+        val original = ByteArrayOutputStream()
+        val sniffingOutput = SniffingOutputStream(original)
+
+        sniffingOutput.write(bytes, 0, half)
+        sniffingOutput.write(bytes, half, bytes.size - half)
+
+        assertArrayEquals(bytes, original.toByteArray())
+    }
+
+    @Test
+    fun sniffed_data_is_equal_to_actual_data_test() {
+        val bytes = data.toByteArray()
+        val half = bytes.size / 2
+
+        val original = OutputStreamMock()
+        val sniffingOutput = SniffingOutputStream(original)
+
+        sniffingOutput.write(bytes, 0, half)
+        sniffingOutput.write(bytes, half, bytes.size - half)
+
+        assertArrayEquals(bytes, sniffingOutput.content)
+    }
+
+    private val data = buildString {
+        appendln("The data this is")
+        appendln("This is a second line")
+        appendln("I will not lose my sniffing streams no more")
+        appendln("One less distraction obscures the skyline")
+    }
+
+    private class OutputStreamMock : OutputStream() {
+        override fun write(b: Int) = Unit
+    }
 }
